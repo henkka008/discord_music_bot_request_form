71c1b76f719f480140a162723751a1cb683efde4
verycube <henkka008@hotmail.com>
2026-02-24 03:23:56 +0200

    fixing bugs

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "dev_change_me"
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["ADMIN_INVITE_CODE"] = os.environ.get("ADMIN_INVITE_CODE", "visaisgreat")
+    app.config["ADMIN_INVITE_CODE"] = os.environ.get("ADMIN_INVITE_CODE")
     
 
     db.init_app(app)
